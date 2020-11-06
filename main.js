b999e3be12eb3a5e8ee1eda4437749b1cd5d04ce
feinoko <feinoko@gmail.com>
2020-11-06 16:35:48 +0100

    functionnal

--- main.js
+++ main.js
@@ -1,35 +1,40 @@
 // let's create a random number between a min & max, user must choose min & max
 
-let min = Number(prompt("pick a minimum"));
-let max = Number(prompt("pick a maximum"));
+let launcher = document.querySelector('p');
 
-function between(min,max) {
-  return Math.floor(
-    Math.random() * (max - min) + min
-  )
+launcher.addEventListener('click', app);
+
+function app() {
+  let min = Number(prompt("pick a minimum"));
+  let max = Number(prompt("pick a maximum"));
+  function between(min,max) {
+    return Math.floor(
+      Math.random() * (max - min) + min
+    )
+  }
+
+  let secretNumber = between(min,max);
+
+  console.log("let's see if you can find me!");
+
+  let guessed = Number(prompt("your guess: "));
+
+  let counter = 1;
+
+  while (guessed != secretNumber) {
+        if (guessed > secretNumber) {
+      alert("wrong, lower!");
+    }
+    if (guessed < secretNumber) {
+      alert("wrong, higher!");
+    }
+    counter += 1;
+    guessed = Number(prompt("your new guess: "));
+  }
+
+  alert("SUCCESS! You have succeeded in " + counter + " attempts!");
 }
 
-let secretNumber = between(min,max);
 
-//console.log("secret number is = " + secretNumber);
 
-console.log("let's see if you can find me!");
 
-let guessed = Number(prompt("your guess: "));
-
-let counter = 1;
-
-while (guessed != secretNumber) {
-  console.log("wrong!");
-  if (guessed > secretNumber) {
-    console.log("lower!");
-  }
-  if (guessed < secretNumber) {
-    console.log("higher!");
-  }
-  counter += 1;
-  guessed = Number(prompt("your new guess: "));
-}
-
-console.log("SUCCESS!");
-console.log("You have succeeded in " + counter + " attempts!");
